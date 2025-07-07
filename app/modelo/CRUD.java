b0d85a1f6df163f5f1345a3c4ac39fda2e0fdb54
mercycheryl <mercysperugachi@gmail.com>
2025-07-07 12:57:00 -0500

    clase

---
+++ app/modelo/CRUD.java
@@ -0,0 +1,22 @@
+package app.modelo;
+
+import java.util.Map;
+
+public interface CRUD {
+    // mostrar todos
+    public Map<Integer, Producto>
+    seleccionarTodo();
+
+    //mostrar uno
+    public Producto buscar(int id);
+
+    //insertar
+    public void insertar(Producto producto);
+
+    //actualizar
+    public void actualizar(Producto producto);
+
+    //eliminar
+    public void eliminar(int id);
+
+}
---
+++ app/modelo/Producto.java
@@ -0,0 +1,33 @@
+package app.modelo;
+
+public class Producto {
+    private int id;
+    private String codigo;
+    private String nombre;
+    private double precio;
+
+    // metodos
+    public Producto(int id, String codigo, String nombre, double precio){
+        this.id=id;
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+
+    public Producto(String codigo, String nombre, double precio){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", codigo='" + codigo + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
