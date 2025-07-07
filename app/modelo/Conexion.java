d546ec216f7c8eae3ae7a7c68022973f56744c02
mercycheryl <mercysperugachi@gmail.com>
2025-07-07 13:00:42 -0500

    clase

---
+++ app/modelo/Conexion.java
@@ -0,0 +1,23 @@
+package app.modelo;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    public Connection conectar(){
+        String url="jdbc:sqlite:"+new File("test.db").getAbsolutePath();
+        Connection conn=null;
+
+        try{
+            conn= DriverManager.getConnection(url);
+            System.out.println("Estamos conectados");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return  conn;
+
+    }
+}
